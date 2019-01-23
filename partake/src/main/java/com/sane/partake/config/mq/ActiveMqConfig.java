1301ae4f0a505af5588ba266add83b5ca8dac9dc
zsy1988cool <zsy1988cool01@gmail.com>
2019-01-23 18:03:14 +0800

    使用rmsTemplate简化activeMq操作

--- partake/src/main/java/com/sane/partake/config/mq/ActiveMqConfig.java
+++ partake/src/main/java/com/sane/partake/config/mq/ActiveMqConfig.java
@@ -1,10 +1,13 @@
 package com.sane.partake.config.mq;
 
+import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.spring.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.ConnectionFactory;
 
@@ -22,9 +25,25 @@ public class ActiveMqConfig {
     private String brokerURL;
 
     @Bean
-    ConnectionFactory connectionFactory() {
+    ActiveMQConnectionFactory targetConnectionFactory() {
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
         activeMQConnectionFactory.setBrokerURL(brokerURL);
         return activeMQConnectionFactory;
+    }
+
+    /*@Bean
+    SingleConnectionFactory connectionFactory(ActiveMQConnectionFactory targetConnectionFactory) {
+        return new SingleConnectionFactory(targetConnectionFactory);
+    }*/
+
+    @Bean
+    ActiveMQQueue activeMQQueue() {
+        ActiveMQQueue activeMQQueue = new ActiveMQQueue("text-msg");
+        return activeMQQueue;
+    }
+
+    @Bean
+    JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+        return new JmsTemplate(connectionFactory);
     }
 }
--- partake/src/main/java/com/sane/partake/service/mq/ActiveMqService.java
+++ partake/src/main/java/com/sane/partake/service/mq/ActiveMqService.java
@@ -1,75 +1,40 @@
 package com.sane.partake.service.mq;
 
-import org.apache.activemq.ActiveMQConnection;
-import org.apache.activemq.ActiveMQMessageConsumer;
-import org.apache.activemq.ActiveMQPrefetchPolicy;
-import org.apache.activemq.RedeliveryPolicy;
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.jms.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ActiveMqService {
 
     @Autowired
-    ConnectionFactory connectionFactory;
+    JmsTemplate jmsTemplate;
 
-    public void sendMessage(String message) throws JMSException
+    @Autowired
+    ActiveMQQueue activeMQQueue;
+    //ConnectionFactory connectionFactory;
+
+    public void sendMessage(String message)
     {
-        Connection connection = connectionFactory.createConnection();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Queue queue = session.createQueue("text-msg");
-
-        MessageProducer messageProducer = session.createProducer(queue);
-        messageProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-
-        TextMessage txtMessage = session.createTextMessage(message);
-        for(int i = 0; i < 3; i++) {
-            messageProducer.send(txtMessage);
-        }
-
-        messageProducer.close();
-        connection.close();
+        jmsTemplate.send(activeMQQueue, session -> {
+            TextMessage textMessage = session.createTextMessage();
+            textMessage.setText(message);
+            return textMessage;
+        });
     }
 
     public List<String> receiveMessage() throws JMSException
     {
-        ActiveMQConnection connection = (ActiveMQConnection)connectionFactory.createConnection();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Queue queue = session.createQueue("text-msg");
-
-        ActiveMQMessageConsumer messageConsumer = (ActiveMQMessageConsumer)session.createConsumer(queue);
-        messageConsumer.start();
-        int aa = messageConsumer.getPrefetchNumber();
-
-        /*
-        messageConsumer.setMessageListener(message -> {
-            TextMessage txtMessage = (TextMessage)message;
-            try {
-                System.out.println("消息接收" + txtMessage.getText());
-            } catch (JMSException e) {
-                e.printStackTrace();
-            }
-        }); */
-
-        List<String> messageList = new ArrayList<>();
-        for(int i = 0; i < 10; i++) {
-            Message message = messageConsumer.receiveNoWait();
-            if(message != null) {
-                TextMessage txtMessage = (TextMessage)message;
-                if(txtMessage != null) {
-                    messageList.add(txtMessage.getText());
-                }
-            }
+        TextMessage textMessage = (TextMessage)jmsTemplate.receiveAndConvert(activeMQQueue);
+        if(textMessage != null) {
+            String txt = textMessage.getText();
+            int i = 0;
+            i++;
         }
-        messageConsumer.stop();
-        messageConsumer.close();
-        connection.close();
-        session.close();
-
-        return messageList;
+        return null;
     }
 }
